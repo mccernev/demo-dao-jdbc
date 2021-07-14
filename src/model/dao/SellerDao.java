86481c3abf51054419e99c06711afaa30c626f82
mccernev <mccernev@yahoo.com.br>
2021-07-13 22:19:26 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,7 +1,6 @@
 package model.dao;
 
 import java.util.List;
-
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,4 +10,5 @@ public interface SellerDao {
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
 }
